6e0436edcf5cad083d98076b674b399bcf573d7d
kynth65 <kynth65@gmail.com>
2025-05-02 08:36:26 +0800

    make the cors more secured

--- config/cors.php
+++ config/cors.php
@@ -2,11 +2,17 @@
 
 return [
     'paths' => ['api/*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], // Add your frontend URL
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    'allowed_origins' => [
+        'https://evergreenrealtyph.com',
+        'https://api.evergreenrealtyph.com',
+        'http://localhost:5173',
+        'http://localhost:8000'
+    ], 
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
     'exposed_headers' => [],
-    'max_age' => 0,
+    'max_age' => 600,
     'supports_credentials' => true,
 ];
+
